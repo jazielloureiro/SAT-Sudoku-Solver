aa9660dda8a69fd13748defe477f7b7a7d99c8fd
jazielloureiro <jazielloureiro17@gmail.com>
2022-02-05 15:22:19 -0300

    Add board reading from file

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,1 +1,14 @@
 #!/bin/python3
+
+def read_board(file):
+    board = []
+
+    with open(file) as data:
+        for line in data:
+            line = line.split(' ')
+            board += [[int(i) for i in line]]
+
+    return board
+
+if __name__ == '__main__':
+    print(read_board('data/sudoku'))
