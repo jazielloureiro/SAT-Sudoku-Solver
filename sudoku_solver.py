78c4681d9b3933c54d238317551a1b527f2b413d
jazielloureiro <jazielloureiro17@gmail.com>
2022-02-05 16:29:32 -0300

    Add grid mapping

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -9,5 +9,24 @@ def read_grid(file):
 
     return grid
 
+def add_clauses():
+    grid_mapping = {}
+    inv_grid_mapping = {}
+    c = 0
+
+    for i in range(1, 4):
+        for j in range(1, 4):
+            for k in range(1, 4):
+                for l in range(1, 4):
+                    for m in range(1, 10):
+                        predicate = 'grid_{}_{}_{}_{}_{}'.format(i, j, k, l, m)
+                        grid_mapping[predicate] = c
+                        inv_grid_mapping[c] = predicate
+                        c += 1
+
+    for i, j in grid_mapping.items():
+        print(i, j)
+
 if __name__ == '__main__':
-    print(read_grid('data/sudoku'))
+    # print(read_grid('data/sudoku'))
+    add_clauses()
